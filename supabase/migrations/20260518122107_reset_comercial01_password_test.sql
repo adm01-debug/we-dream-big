5016cd0ad55280f0a6a93357a0c2614bed28e728
adm01-debug <adm01@promobrindes.com.br>
2026-05-23 20:52:12 -0300

    Fix semantic search loop and remove insecure password migration

--- supabase/migrations/20260518122107_reset_comercial01_password_test.sql
+++
@@ -1,4 +0,0 @@
--- Reset cirurgico: comercial01 para teste
-UPDATE auth.users
-SET encrypted_password = crypt('@Promobrindes2021', gen_salt('bf')), updated_at = now()
-WHERE email = 'comercial01@promobrindes.com.br';
